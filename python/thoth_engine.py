10fed5d50cb41d7a4b67a3656880828f7d1b1edf
Max Yu <maxyu1115@yahoo.com>
2021-09-18 01:16:47 -0500

    Thoth Engine glue code

--- python/thoth_engine.py
+++ python/thoth_engine.py
@@ -1,5 +1,9 @@
 import argparse
 import os
+import cv2
+import pipeline
+import util
+import ocr
 
 parser = argparse.ArgumentParser(description='Run the thoth pipeline. ')
 parser.add_argument('--vid', type=str, help='location of the video')
@@ -8,10 +12,33 @@ parser.add_argument('--target', type=str, help='location of output folder')
 args = parser.parse_args()
 
 
+def setupPipeline():
+    processor = pipeline.Pipeline()
+    # TODO: add std detect here
+
+    processor.addOperation(ocr.OCR())
+
+    # TODO: add speech2text here
+    # TODO: add whoosh indexing here
+    return processor
+
+
 def main():
     # do stuff here
-    with open(os.path.join(args.target, "your_file.txt"), 'w') as f:
-        f.write("TEST!!!\n")
+    # with open(os.path.join(args.target, "your_file.txt"), 'w') as f:
+    #     f.write("TEST!!!\n")
+
+    # setup pipeline
+    processor = setupPipeline()
+
+    locator = util.FileLocator(os.path.basename(args.vid), args.target)
+
+    video = cv2.VideoCapture(args.vid)
+    processor.processVideo(video, locator)
+
+    # clean up
+    video.release()
+    cv2.destroyAllWindows()
     return
 
 
