06ee15265a7439d195d1c093244544e4cbcf8022
Victor Song <vms2@rice.edu>
2021-09-18 10:32:12 -0500

    Add dropzone

--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -3,16 +3,21 @@ import { execFile } from 'child_process';
 import path from 'path';
 import { promisify } from 'util';
 
-export const execFilePromise = promisify(execFile);
+const execFilePromise = promisify(execFile);
 
 // eslint-disable-next-line @typescript-eslint/promise-function-async
-const runThoth = (filepath: string): ReturnType<typeof execFilePromise> => {
+export const runThoth = (
+  filepath: string,
+): ReturnType<typeof execFilePromise> => {
   const command = path.join(process.resourcesPath, 'thoth_engine');
   const args = ['--target', filepath];
 
   return execFilePromise(command, args);
 };
 
+export const getDirname = path.dirname;
+
 contextBridge.exposeInMainWorld('thoth', {
   runThoth: runThoth,
+  getDirname: getDirname,
 });
