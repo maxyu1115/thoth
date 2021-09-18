d214ed7ad2d369b9729a4a5992684c3da58028a3
kc-luo <luokaichun@outlook.com>
2021-09-18 16:34:39 -0500

    Changed selection method

--- python/slideDetect.py
+++ python/slideDetect.py
@@ -9,6 +9,7 @@ from typing import List, Tuple
 from customDetector import StdDetector
 from scenedetect.detectors import ContentDetector, ThresholdDetector
 
+import numpy as np
 import cv2
 
 import util
@@ -22,9 +23,9 @@ class SlideDetect(pipeline.ProcessingOperation):
         assert video_type in ["animated_slides", "unanimated_slides"]
         super(SlideDetect, self).__init__()
         if video_type == "animated_slides":
-            self.detector = StdDetector(skip=10, window=10, threshold=10, average=1)
+            self.detector = StdDetector(skip=50, window=10, threshold=10, average=20)
         elif video_type == "unanimated_slides":
-            self.detector = ContentDetector(threshold=10)
+            self.detector = ContentDetector(threshold=4, luma_only=True)
 
     def set_detector(self, new_detector):
         self.detector = new_detector
@@ -32,8 +33,11 @@ class SlideDetect(pipeline.ProcessingOperation):
     def process(self, file_locator: FileLocator):
         print("Processing video: ", file_locator.file_pathname)
         scenes, stats = self._find_scenes(file_locator.file_pathname)
-        frame_lst, output = self._select_frames(scenes, stats, file_locator, frame_method="smart")
-        self.export_frames(file_locator, frame_lst)
+        # frame_lst, output = self._select_frames(scenes, stats, file_locator, frame_method="middle")
+
+        frame_lst = [x[1].get_frames() for x in scenes]
+        # print(frame_lst)
+        output = self.export_frames(file_locator, frame_lst)
         # write data to json file
         with open(file_locator.getDetectJsonName(), "w") as write_file:
             json.dump(output, write_file)
@@ -55,24 +59,9 @@ class SlideDetect(pipeline.ProcessingOperation):
         # Each returned scene is a tuple of the (start, end) timecode.
         return scene_manager.get_scene_list(), stats_manager
 
-    def _select_frames(self, scenes: List[Tuple[FrameTimecode, FrameTimecode]],
-                       stats_manager: StatsManager, file_locator: FileLocator, frame_method: str):
-        output = []
-        frame_lst = []
-        for s, f in scenes:
-            start_time = int(s.get_seconds() * 1000)
-            end_time = int(f.get_seconds() * 1000)
-            image_frame = self.get_best_frame(s.get_frames(), f.get_frames(), stats_manager, frame_method)
-            frame_lst.append(image_frame)
-
-            image_time = int(image_frame * 1000 / s.get_framerate())
-            image_path = file_locator.getScreenshotName(image_time)
-            output.append(util.make_detect_dict(start_time, end_time, image_time, image_path))
-
-        return frame_lst, output
-
     @staticmethod
     def export_frames(file_locator: FileLocator, frames: List[int]):
+        print("Start exporting images...")
         cap = cv2.VideoCapture(file_locator.file_pathname)
 
         # Find OpenCV version and then get FPS
@@ -82,35 +71,54 @@ class SlideDetect(pipeline.ProcessingOperation):
         else:
             fps = cap.get(cv2.CAP_PROP_FPS)
 
-        i = 0
-        while cap.isOpened() and i < len(frames):
-            frame_num = frames[i]
-            cap.set(1, frame_num)
+        output = []
+        frame_num = 0
+        slice_num = 0
+        top_frame_count = 0
+        curr_frame_count = 0
+        top_frame = None
+        top_frame_idx = -1
+        curr_frame = None
+        while cap.isOpened() and slice_num < len(frames):
+
+            if frame_num == frames[slice_num]:
+                # End of a video slice
+                start_time = int(frames[slice_num - 1] * 1000 / fps) if slice_num > 0 else 0
+                end_time = int(frame_num * 1000 / fps)
+                image_time = int(top_frame_idx * 1000 / fps)
+                image_path = file_locator.getScreenshotName(image_time)
+                # print(image_path, top_frame)
+                cv2.imwrite(image_path, top_frame)
+                output.append(util.make_detect_dict(start_time, end_time, image_time, image_path))
+                slice_num += 1
+                top_frame_count = 0
+                curr_frame_count = 0
+                top_frame = None
+                top_frame_idx = -1
+
             ret, frame = cap.read()
             if not ret:
                 break
-            out_path = file_locator.getScreenshotName(frame_num * 1000 // fps)
-            cv2.imwrite(out_path, frame)
-            i += 1
+            frame_num += 1
+
+            if top_frame is None:
+                top_frame = frame
+                top_frame_idx = frame_num
+
+            if np.sum(1 - np.equal(frame, curr_frame)) < (frame.size * 0.25):
+                curr_frame_count += 1
+            else:
+                if curr_frame_count >= top_frame_count:
+                    top_frame = curr_frame
+                    top_frame_count = curr_frame_count
+                    top_frame_idx = frame_num - curr_frame_count
+                curr_frame = frame
+                curr_frame_count = 1
 
         cap.release()
         cv2.destroyAllWindows()
 
-    def get_best_frame(self, start_frame: int, end_frame: int, stats_manager: StatsManager, frame_method="smart"):
-        if frame_method == "last":
-            return end_frame
-        elif frame_method == "first":
-            return start_frame
-        elif frame_method == "middle":
-            return (start_frame + end_frame) // 2
-        elif frame_method == "smart":
-            valid_frames = []
-            for frame, val_dict in stats_manager._frame_metrics.items():
-                if start_frame <= frame < end_frame:
-                    score = val_dict[self.detector.FRAME_SCORE_KEY]
-                    if score is not None:
-                        valid_frames.append((frame, score))
-            return sorted(valid_frames, key=lambda x: (x[0], -x[1]))[0][0]
+        return output
 
 
 # Testing slideDetect
@@ -118,6 +126,6 @@ import os
 
 if __name__ == "__main__":
     filename = "test/SlideChangeTest1.mov"
-    locator = FileLocator(filename, os.getcwd() + "/output3/temp")
+    locator = FileLocator(filename, os.getcwd() + "/output66/temp")
     detect = SlideDetect("unanimated_slides")
     detect.process(locator)
