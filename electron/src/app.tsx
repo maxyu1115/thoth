aa624cf52a3341741b98b327bf7cba733ed25202
Victor Song <vms2@rice.edu>
2021-09-18 00:53:04 -0500

    Added linting and formatting to frontend

--- electron/src/app.tsx
+++ electron/src/app.tsx
@@ -1,31 +1,45 @@
 import { FormEvent, useState } from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { execFilePromise } from './preload';
 
 declare global {
-    interface Window { thoth: { runThoth: (path: string) => Promise<{
-        stdout: string;
-        stderr: string;
-    }> } }
-}
-
-const App = () => {
-    const [fileName, setFileName] = useState("");
-    const [output, setOutput] = useState("");
-
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        window.thoth.runThoth(fileName).then(result => setOutput(result.stdout));
+  interface Window {
+    thoth: {
+      runThoth: (path: string) => ReturnType<typeof execFilePromise>;
     };
-
-    return (
-        <>
-        <form onSubmit={handleSubmit}>
-            <label>Enter file: <input type="text" value={fileName} onChange={e => setFileName(e.target.value)} /></label>
-            <input type="submit" value="Submit" />
-        </form>
-        {output != "" && <p>{output}</p>}
-        </>
-    );
+  }
 }
 
-render(<App />, document.getElementById("app"));
+const App = (): JSX.Element => {
+  const [fileName, setFileName] = useState('');
+  const [output, setOutput] = useState('');
+
+  const handleSubmit = (e: FormEvent): void => {
+    e.preventDefault();
+    window.thoth
+      .runThoth(fileName)
+      .then((result) => setOutput(result.stdout.toString()))
+      .catch((reason) =>
+        setOutput(`Command failed: ${JSON.stringify(reason)}`),
+      );
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter file:{' '}
+          <input
+            type="text"
+            value={fileName}
+            onChange={(e) => setFileName(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {output !== '' && <p>{output}</p>}
+    </>
+  );
+};
+
+render(<App />, document.getElementById('app'));
--- electron/src/preload.ts
+++ electron/src/preload.ts
@@ -1,18 +1,18 @@
-import { contextBridge } from "electron";
+import { contextBridge } from 'electron';
 import { execFile } from 'child_process';
 import path from 'path';
 import { promisify } from 'util';
- 
-const execFilePromise = promisify(execFile);
 
-const runThoth = (filepath: string) => {
-    const command = path.join(process.resourcesPath, "thoth_engine");
-    const args = ["--target", filepath];
+export const execFilePromise = promisify(execFile);
 
-    return execFilePromise(command, args);
-}
+// eslint-disable-next-line @typescript-eslint/promise-function-async
+const runThoth = (filepath: string): ReturnType<typeof execFilePromise> => {
+  const command = path.join(process.resourcesPath, 'thoth_engine');
+  const args = ['--target', filepath];
+
+  return execFilePromise(command, args);
+};
 
 contextBridge.exposeInMainWorld('thoth', {
-    desktop: true,
-    runThoth: runThoth,
-})
+  runThoth: runThoth,
+});
